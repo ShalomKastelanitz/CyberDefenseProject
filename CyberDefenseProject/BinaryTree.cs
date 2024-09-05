c8d58cb324e4909d809afdf876faceae569f26d3
This User <This User@DESKTOP-L9S4R74>
2024-09-05 12:29:45 +0300

    Add project files.

---
+++ CyberDefenseProject/BinaryTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberDefenseProject
+{
+    internal class BinaryTree
+    {
+        public NodeDefense root;
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public void Insert(NodeDefense Defense)
+        {
+            if (root == null)
+            {
+                root = Defense;
+                return;
+            }
+            else
+            {
+
+                Insert(root, Defense);
+
+            }
+        }
+        private void Insert(NodeDefense node, NodeDefense Defense)
+        {
+            if ((Defense.MinSeverity+Defense.MaxSeverity) < (node.MinSeverity+node.MaxSeverity))
+            {
+                if (node.Left != null)
+                {
+                    Insert(node.Left, Defense);
+                }
+                else
+                {
+                    node.Left = Defense;
+                    return;
+                }
+
+            }
+            else
+            {
+                if (node.Right != null)
+                {
+                    Insert(node.Right, Defense);
+                }
+                else
+                {
+                    node.Right = Defense;
+                    return;
+                }
+            }
+
+        }
+       
+    }
+}
---
+++ CyberDefenseProject/NodeDefense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberDefenseProject
+{
+    internal class NodeDefense
+    {
+        public int MinSeverity { get; set; }
+
+        public int MaxSeverity { get; set; }
+        public List<string> Defenses { get; set; }
+        public NodeDefense Left { get; set; }
+        public NodeDefense Right { get; set; }
+
+
+
+
+    }
+
+}
